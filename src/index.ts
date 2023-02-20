ecb651b9d0e3f8a4775e2ddbe32e463cafbb645d
Troy Chaplin <troy.chaplin@gmail.com>
2023-02-20 12:01:55 -0500

    Fixed index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { Button } from "./components/Button/Button";
+// export { Button } from "./components/Button/Button";
 export { Card } from "./components/Card/Card";
-export { Header } from "../comp-bck/Header/Header";
+// export { Header } from "../components/Header/Header";
