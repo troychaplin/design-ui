b2f08edfbda78a5b9ccdbe775c6ac6a94e38d45c
Troy Chaplin <troy.chaplin@gmail.com>
2023-02-20 10:56:42 -0500

    Added docs addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,9 +2,18 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
 	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 	addons: [
-		"@storybook/addon-links",
+		"@storybook/addon-actions",
 		"@storybook/addon-essentials",
 		"@storybook/addon-interactions",
+		"@storybook/addon-links",
+		{
+			name: "@storybook/addon-docs",
+			options: {
+				configureJSX: true,
+				babelOptions: {},
+				transcludeMarkdown: true,
+			},
+		},
 	],
 	framework: {
 		name: "@storybook/react-vite",
