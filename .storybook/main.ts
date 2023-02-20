013dcea4eb5a61fd04ccfd58442c674615f5901e
Troy Chaplin <troy.chaplin@gmail.com>
2023-02-20 11:32:58 -0500

    Updated outline and toolbars

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,6 @@ const config: StorybookConfig = {
 	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 	addons: [
 		"@storybook/addon-actions",
-		"@storybook/addon-essentials",
 		"@storybook/addon-interactions",
 		"@storybook/addon-links",
 		{
@@ -12,6 +11,13 @@ const config: StorybookConfig = {
 				configureJSX: true,
 				babelOptions: {},
 				transcludeMarkdown: true,
+			},
+		},
+		{
+			name: "@storybook/addon-essentials",
+			options: {
+				outline: false,
+				toolbars: false,
 			},
 		},
 	],
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,7 +2,25 @@ import "../src/index.css";
 export const parameters = {
 	actions: { argTypesRegex: "^on[A-Z].*" },
 	backgrounds: {
-		default: "light",
+		default: "white",
+		values: [
+			{
+				name: "white",
+				value: "#ffffff",
+			},
+			{
+				name: "light grey",
+				value: "#f2f2f2",
+			},
+			{
+				name: "dark grey",
+				value: "#333333",
+			},
+			{
+				name: "black",
+				value: "#000000",
+			},
+		],
 	},
 	controls: {
 		expanded: true,
