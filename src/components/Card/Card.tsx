5564b4993d495db3661f4bafb2a32f779a331274
Troy Chaplin <troy.chaplin@gmail.com>
2023-02-20 21:24:19 -0500

    Added eslint and prettier

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,23 +1,23 @@
-import styles from "./Card.styles";
+import styles from './Card.styles'
 
 export interface CardProps {
-	/**
-	 * What is the title for this card?
-	 */
-	title: string;
-	/**
-	 * Add an excerpt for the card
-	 */
-	description: string;
+    /**
+     * What is the title for this card?
+     */
+    title: string
+    /**
+     * Add an excerpt for the card
+     */
+    description: string
 }
 
 export const Card = ({ title, description }: CardProps) => {
-	return (
-		<div className={styles.card}>
-			<div className={styles.spacing}>
-				<h2 className={styles.header}>{title}</h2>
-				<p className={styles.excerpt}>{description}</p>
-			</div>
-		</div>
-	);
-};
+    return (
+        <div className={styles.card}>
+            <div className={styles.spacing}>
+                <h2 className={styles.header}>{title}</h2>
+                <p className={styles.excerpt}>{description}</p>
+            </div>
+        </div>
+    )
+}
